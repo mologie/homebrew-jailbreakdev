2f02e6d36f892cc62896ab054d5d53466157d0a1
Oliver Kuckertz <oliver.kuckertz@mologie.de>
2014-12-19 14:43:18 +0100

    Add commit hash to deviceconsole formula

--- deviceconsole.rb
+++ deviceconsole.rb
@@ -2,9 +2,8 @@ require "formula"
 
 class Deviceconsole < Formula
   homepage "https://github.com/rpetrich/deviceconsole"
-  
-  head "https://github.com/rpetrich/deviceconsole.git"
-  
+  head "https://github.com/rpetrich/deviceconsole.git", :revision => "4d3c4b978ab3cdf73d33c9d41817c031eeac03e9"
+
   def install
     system "make"
     bin.install "deviceconsole"
